43913af834cb002042aa6981182a12dab15f4f50
DevTec2020 <leonardo.silva.inf@gmail.com>
2025-01-04 10:50:30 -0300

    feat: adicionando useState para criar um contador

--- classroom/src/App.tsx
+++ classroom/src/App.tsx
@@ -1,14 +1,30 @@
 import "./global.css"
+import { useState } from "react"
+
 import { Button } from "./components/button"
+// import { useMessage } from "./hooks/useMessage"
 
 import styles from "./app.module.css"
 
+
 export function App() {
+     const [count, setCount] = useState(0)
+    // const message = useMessage()
+
+
+    function handleAdd() {
+        setCount(count +1)
+    }
+
+    function handleRemove() {
+        setCount(count -1)
+    }
+
     return (
         <div className={styles.container}>
-            <Button name="Adicionar" onClick={() => alert("Adicionado")}/>
-            <span>0</span>
-            <Button name="Remover"/>
+            <Button name="Adicionar" onClick={ handleAdd } />
+            <span>{ count }</span>
+            <Button name="Remover" onClick={ handleRemove} />
         </div>
     )
 }
---
+++ classroom/src/hooks/useMessage.ts
@@ -0,0 +1,7 @@
+export function useMessage() {
+    function show() {
+        console.log("Messagem do meu próprio Hook!")
+    }
+
+    return { show }
+}
